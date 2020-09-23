b1dd1057c5fe3d413374b2e6e686dde7740e287f
Irina-Kovtun <irinakovtun.us@gmail.com>
2020-09-24 01:27:47 +0300

    Manager

--- src/main/java/ru/netology/domain/PostSource.java
+++ src/main/java/ru/netology/domain/PostSource.java
@@ -2,4 +2,14 @@ package ru.netology.domain;
 
 public class PostSource {
     private boolean standaloneApp;
+
+    public boolean isStandaloneApp() {
+        return standaloneApp;
+    }
+
+    public void setStandaloneApp(boolean standaloneApp) {
+        this.standaloneApp = standaloneApp;
+    }
+
+
 }
